7c17c0e2e1651cdaeca622ec3568e853738df991
nathan-coleman <PGC-624184@proton.me>
2023-09-22 14:49:45 +1000

    Orbitals visualisation for the Ca H2 system

--- Quantum_Chem.jl
+++ Quantum_Chem.jl
@@ -1,131 +1,16 @@
-using Fermi
-using Fermi.Integrals
 using Plots
 using ProgressMeter
-using Molecules
 using Unitful
 using PhysicalConstants.CODATA2018:a_0,e,ε_0,ħ,c_0,α,h,k_B
 
-## This section calculates the potential energy curve for the ground state
-# of the CaH system (as in the Habli et al Paper)
 
-# Distance vector for system
-Rval2 = round.([0.99 + 0.01*i for i = 1:901],digits=2)
-# results vector
-E_rhf = []
 
-@showprogress for r in Rval2
-  # Set the CaH system
-  mol = string("""
-  Ca          0.0 0.0 0.0
-  H           $r  0.0    0.0
-  """);
-
-  # settings for solve
-  @set {
-    basis cc-pvtz # coupled cluster doublet basis
-    scf_max_iter	100 # iter max out
-    DIIS true # convergence tool
-    charge -1 # Charge of the system (free e⁻)
-    multiplicity 3 # multiplicity of the molecule (2n+1)
-  }
-
-  Fermi.Options.set("molstring", mol);
-  
-  # Calc the energy at a given radius
-  rhf_e = @energy rhf;
-
-  # Save result, noting in atomic units
-  push!(E_rhf, rhf_e.energy)
-
-end
 
 # Energy in atomic units to eV
 E_h = (ħ*c_0*α)/(a_0) |> u"eV"
 
 # Add units to the radius
 new_r = (Rval2)u"angstrom"
-
-# Scale change to indicate attraction/repulsion 
-E_CaH = (E_rhf).*E_h .|> u"keV"#.-E_rhf[end]).*E_h
-
-# plot
-plot(new_r,E_CaH,label="Reduced Hartree Fock CaH X²Σ⁺",xlabel="R",ylabel="E")
-
-
-
-
-## This section considers the Ca H₂ system which seems to be case in Proxima cen
-rhf_min_CaH,rhf_idx = findmin(E_CaH)
-rhf_sep = new_r[rhf_idx]
-
-
-plot!(new_r[1:end-1],diff(E_CaH))
-Rval = round.([0.9 + 0.01*i for i = 1:191],digits=2)
-E_H_2 = []
-@reset
-@showprogress for r in Rval2
-  # Set the CaH system
-  mol = string("""
-  Ca          0.0   0.0     0.0
-  H           $r    0.36    0.0
-  H           $r   -0.36    0.0
-  """);
-
-  # settings for solve
-  @set {
-    basis cc-pvtz
-    scf_max_iter	100
-    DIIS true # helper for convergence
-    reference rhf #Restricted Hartree-Fock
-    charge 0 # Charge of the system
-    multiplicity 1 # multiplicity of the molecule
-  }
-  Fermi.Options.set("molstring", mol);
-  #I = Fermi.Integrals.IntegralHelper(eri_type=Fermi.Integrals.Chonky())
-  # Calc the energy at a given radius
-  wf = @energy rhf;
-  # Save result, noting E_h is in Hartrees
-  push!(E_H_2, wf.energy)
-end
-
-#new_r = (Rval[1:end]u"angstrom")./a_0 .|> upreferred
-E_CaH2 = (E_H_2[1:end]).*E_h .|> u"keV"#- E_H_2[end])*E_h
-plot!(new_r,E_CaH2,label="CaH₂",xlabel="R(a.u.)")
-
-
-new_test = (E_CaH .- E_CaH2) .|> u"eV"
-
-spectrum = c_0./(new_test./h) .|> u"nm"
-
-plot(spectrum)
-
-
-
-
-
-
-
-@reset
-@molecule {
-  Ca 0 0 0
-}
-@set {
-  basis cc-pvtz
-  charge -2
-  multiplicity 5
-}
-
-Ca_single = @energy rhf
-Ca_single.energy
-
-
-
-
-
-
-
-
 
 
 ## Data points from the Habli paper
@@ -134,15 +19,15 @@ curves = [[17.454 -0.000068182],
 [17.454 0.10670]]
 
 # distance between points out at 10 a.u.
-dist = abs(curves[3][2]-curves[1][2])
+dist = abs(curves[2][2]-curves[1][2])
+dist2 = abs(curves[3][2]-curves[1][2])
 
 # Energy in Hartrees
 E_h = (ħ*c_0*α)/(a_0) |> u"eV"
 
-
-dist = 0.0940738008715698
 # Line core for CaH 
 λ_habli = c_0*h/((dist*E_h)) |> u"nm"
+λ_habli2 = c_0*h/((dist2*E_h)) |> u"nm"
 
 c_0/((2.9u"eV")/h) |> u"nm"
 
@@ -151,9 +36,6 @@ c_0/((2.9u"eV")/h) |> u"nm"
 
 # distance check using NIST value
 dist_1 = c_0*h/(λ*E_h) |> upreferred
-
-# This provides the line core, lorentzian wings from the sum of the transition probabilities
-
 
 ## Taking the EOS code that Mike Ireland developed to understand Debye shielding and interatomic distances
 T = ([1500.0, 1660.0, 1930.0, 2350, 3320])u"K"
--- transition_density_pyscf.py
+++ transition_density_pyscf.py
@@ -3,35 +3,36 @@ from pyscf import gto, scf, mcscf
 from pyscf import gto, mp, mcscf, dft, tddft
 from pyscf.mcscf import dmet_cas
 from pyscf import gto, scf, cc
+import matplotlib.pyplot as plt
 
 # Bond seperation lengths in angstrom
 bond = np.arange(1.0, 10.0, 0.05)
 dm_init = None
 
 # results list, includes ground state and excited states
-e_hf = [[],[]]
+e_hf = [[],[],[]]
 
 # Main iteration loop
 for j in range(0,3): # iterate over excited states
     for r in reversed(bond): # iterate over bond lengths. Start out and work in for stability
         # Define the CaH molecule with the ccpvdz basis set
-        mol = gto.M(atom=[['Ca', 0, 0, 0],
-                          ['H', r, 0, -0.3865],
-                          ['H' ,r, 0 , 0.3865]],
+        mol = gto.M(atom=[['Ca', 0.0, 0.0, 0.0],
+                          #['H', r, 0, -0.3865],
+                          ['H' ,r, 0.0 , 0.0]],
                       basis='ccpvdz',
-                      spin = 0
+                      spin = 1
                     )
         # Reduced Hartree Fock solution as an initial case           
-        mf = scf.RHF(mol).run()
+        mf = scf.UHF(mol).run()
         # define the excited state
         state_id = j
         # For the excited states, need to change the solver
         if state_id > 0:
-            ao_labels = ['Ca 4pz', 'Ca 4py', 'Ca 4s',' Ca 4px']
+            ao_labels = ['Ca 4pz', 'Ca 4py', 'Ca 4s',' Ca 4px', 'Ca 5s', 'Ca 5pz', 'Ca 5py',' Ca 5px']
             ncas, nelecas, mo = dmet_cas.guess_cas(mf, mf.make_rdm1(), ao_labels)
             mycas = mcscf.CASSCF(mf, ncas, nelecas).state_specific_(state_id)
             mycas.verbose = 2
-            mycas.frozen = 6 # Freeze the inner 6 orbitals to save on computational effort
+            mycas.frozen = 6 # Freeze the inner 6 orbitals to save on computational effort (1s 2s 2p[xyz] 3s 3p[xyz] 4s)
             res = mycas.kernel(mo)
             #mycas.analyze()
             e_hf[j].append(res[0]) # append results
@@ -41,6 +42,24 @@ for j in range(0,3): # iterate over excited states
             e_hf[j].append(mycc.e_tot+et)
 
 
+results = []
+from pyscf import gto, scf, dft, tddft
+bond = np.arange(1.0, 6.0, 0.5)
+for r in reversed(bond):
+    mol = gto.M(atom=[['Ca', 0.0, 0.0, 0.0],
+                      ['H' ,r, 0.0 , 0.0]],
+                        basis='ccpvdz',
+                        spin = 1,
+                        verbose = 4)
+    mf = dft.RKS(mol)
+    mf.xc = 'b3lyp'
+    mf.kernel()
+
+    mytd = tddft.TDDFT(mf)
+    mytd.nstates = 5
+    res = mytd.kernel()
+    mytd.analyze()
+    results.append(res[0])
 
 radius = bond # distance to atomic units from angstrom
 # Convert the below to a difference from the far field ground state
@@ -55,7 +74,7 @@ third_state = 2*(e_hf[3][::-1]-e_hf[0][1])*E_h
 # plots of the system
 from matplotlib import pyplot as plt
 plt.clf()
-plt.plot(radius, ground_state,label="Ground State")
+plt.plot(radius, ground_state[0:-3],label="Ground State")
 plt.plot(radius, first_state,label="1st State")
 plt.plot(radius, second_state,label="2nd State")
 plt.plot(radius, third_state,label="3rd State")
@@ -97,3 +116,84 @@ plt.plot(bond, lambda_3[::-1],label="3rd to Ground")
 plt.legend()
 plt.show()
 plt.savefig("Lambda_spread.png")
+
+
+r = 27.83897594220896
+r = 10
+from functools import reduce
+import numpy
+from pyscf import gto, scf, mcscf, fci
+mol = gto.M(atom=[  ['Ca', 0,     0,     0],
+                    ['H',10,     0,    -0.3865],
+                    ['H',10,     0,    0.3865]],
+                    basis='def2-tzvp',
+                    spin = 0,
+                    verbose = 4)
+        # Reduced Hartree Fock solution as an initial case           
+mf = scf.RHF(mol)
+mf.kernel()
+#mf.analyze()
+
+#
+# 1. State-average CASSCF to get optimal orbitals
+#
+mc = mcscf.CASSCF(mf, 6, 4)
+solver_ag = fci.direct_spin0_symm.FCI(mol)
+solver_b2u = fci.direct_spin0_symm.FCI(mol)
+solver_b2u.wfnsym = 'B2u'
+mc = mcscf.state_average_mix(mc, [solver_ag,solver_b2u], [.5,.5])
+cas_list = [17,20,21,22,23,30]  # 2pz orbitals
+mo = mcscf.sort_mo(mc, mf.mo_coeff, cas_list)
+mc.kernel(mo)
+#mc.analyze()
+mc_mo = mc.mo_coeff
+
+#
+# 2. Ground state wavefunction.  This step can be passed you approximate it
+# with the state-averaged CASSCF wavefunction
+#
+mc = mcscf.CASCI(mf, 6, 6)
+mc.fcisolver.wfnsym = 'Ag'
+mc.kernel(mc_mo)
+ground_state = mc.ci
+
+#
+# 3. Exited states.  In this example, B2u are bright states.
+#
+# Here, mc.ci[0] is the first excited state.
+#
+mc = mcscf.CASCI(mf, 6, 6)
+mc.fcisolver.wfnsym = 'B2u'
+mc.fcisolver.nroots = 8
+mc.kernel(mc_mo)
+
+#
+# 4. transition density matrix and transition dipole
+#
+# Be careful with the gauge origin of the dipole integrals
+#
+charges = mol.atom_charges()
+coords = mol.atom_coords()
+nuc_charge_center = numpy.einsum('z,zx->x', charges, coords) / charges.sum()
+mol.set_common_orig_(nuc_charge_center)
+dip_ints = mol.intor('cint1e_r_sph', comp=3)
+
+def makedip(ci_id):
+    # transform density matrix in MO representation
+    t_dm1 = mc.fcisolver.trans_rdm1(ground_state, mc.ci[ci_id], mc.ncas, mc.nelecas)
+    # transform density matrix to AO representation
+    orbcas = mc_mo[:,mc.ncore:mc.ncore+mc.ncas]
+    t_dm1_ao = reduce(numpy.dot, (orbcas, t_dm1, orbcas.T))
+    # transition dipoles
+    return numpy.einsum('xij,ji->x', dip_ints, t_dm1_ao)
+
+# 1st and 6th excited states are B2u of D6h point group, dark states
+# 3rd and 4th excited states are triplet states, dipole == 0
+for i in range(8):
+    print('Transition dipole between |0> and |%d>'%(i+1), makedip(i))
+
+
+from pyscf.tools import cubegen
+# Save Orbitals
+for j in range(0,48):
+    cubegen.orbital(mol, 'CaH2_mo_'+str(j)+'.cub', myhf.mo_coeff[:,j])
